99c5cc0013b78a14651f5070d611ef68fc3401ca
2585924348 <2585924348@qq.com>
2019-12-12 23:39:41 +0800

    add 'fuzzy reward'

--- impedance_controller.py
+++ impedance_controller.py
@@ -26,12 +26,17 @@ class ImpedanceController:
         self.B = np.diag((160, 160, 800, 16, 16, 16))
         self.v = [0, 0, 0, 0, 0, 0]
         self.vd = [0, 0, 0, 0, 0, 0]
+        self.a = [0, 0, 0, 0, 0, 0]
         self.Tc = 0.008
         self.MNum = np.linalg.inv(self.M+self.B*self.Tc)
         self.target_ft = [0, 0, 15, 0, 0, 0]
         self.target_ft = np.reshape(self.target_ft, (6, 1))
         self.v = np.reshape(self.v, (6, 1))
         self.vd = np.reshape(self.vd, (6, 1))
+        self.a = np.reshape(self.a, (6, 1))
+        self.z = 0
+        self.dz = 0
+        self.lastz = 0
 
     def AxisAng2RotaMatri(self, angle_vec):
         '''
@@ -78,6 +83,239 @@ class ImpedanceController:
         axis_ang = np.array([theta*e1, theta*e2, theta*e3])
         return axis_ang
 
+    def get_zdz(self):
+        self.z = abs(self.robot.get_actual_tcp_pose()[2]-self.initz)
+        self.dz = self.z-self.lastz
+        self.lastz = self.z
+        return self.z, self.dz
+
+    def fuzzy_reward(self, f, m, z, dz):
+        mfoutput = self.fuzzy_mf(f, m)
+        zdzoutput = self.fuzzy_zdz(z, dz)
+        reward = self.fuzzy_2(mf, zdz)
+        return reward
+
+    def fuzzy_2(self, mf, zdz):
+        secnum = 4
+        mf_max = 1
+        everysecmf = mf_max / secnum
+        secmf1 = everysecmf
+        secmf2 = 2 * everysecmf
+        secmf3 = 3 * everysecmf
+
+        mf1 = 1 - mf / everysecmf
+        mf2 = 1 - abs(mf - secmf1) / everysecmf
+        mf3 = 1 - abs(mf - secmf2) / everysecmf
+        mf4 = 1 - abs(mf - secmf3) / everysecmf
+        mf5 = (mf - secmf3) / everysecmf
+
+        zdz_max = 1
+        everyseczdz = zdz_max / secnum
+        seczdz1 = everyseczdz
+        seczdz2 = 2 * everyseczdz
+        seczdz3 = 3 * everyseczdz
+
+        zdz1 = 1 - zdz / everyseczdz
+        zdz2 = 1 - abs(zdz - seczdz1) / everyseczdz
+        zdz3 = 1 - abs(zdz - seczdz2) / everyseczdz
+        zdz4 = 1 - abs(zdz - seczdz3) / everyseczdz
+        zdz5 = (zdz - seczdz3) / everyseczdz
+
+        mf1 = max(min(mf1, 1.0), 0.0)
+        mf2 = max(min(mf2, 1.0), 0.0)
+        mf3 = max(min(mf3, 1.0), 0.0)
+        mf4 = max(min(mf4, 1.0), 0.0)
+        mf5 = max(min(mf5, 1.0), 0.0)
+
+        zdz1 = max(min(zdz1, 1.0), 0.0)
+        zdz2 = max(min(zdz2, 1.0), 0.0)
+        zdz3 = max(min(zdz3, 1.0), 0.0)
+        zdz4 = max(min(zdz4, 1.0), 0.0)
+        zdz5 = max(min(zdz5, 1.0), 0.0)
+
+        r1 = min(mf1, zdz1)
+        r2 = min(mf1, zdz2)
+        r3 = min(mf1, zdz3)
+        r4 = min(mf1, zdz4)
+        r5 = min(mf1, zdz5)
+        r6 = min(mf2, zdz1)
+        r7 = min(mf2, zdz2)
+        r8 = min(mf2, zdz3)
+        r9 = min(mf2, zdz4)
+        r10 = min(mf2, zdz5)
+        r11 = min(mf3, zdz1)
+        r12 = min(mf3, zdz2)
+        r13 = min(mf3, zdz3)
+        r14 = min(mf3, zdz4)
+        r15 = min(mf3, zdz5)
+        r16 = min(mf4, zdz1)
+        r17 = min(mf4, zdz2)
+        r18 = min(mf4, zdz3)
+        r19 = min(mf4, zdz4)
+        r20 = min(mf4, zdz5)
+        r21 = min(mf5, zdz1)
+        r22 = min(mf5, zdz2)
+        r23 = min(mf5, zdz3)
+        r24 = min(mf5, zdz4)
+        r25 = min(mf5, zdz5)
+
+        r0 = r1+r2+r3+r4+r5+r6+r7+r8+r9+r10+r11+r12+r13+r14+r15+r16+r17+r18+r19+r20+r21+r22+r23+r24+r25
+
+        output = (0.33*(r1+r6)+0.48*(r2+r7+r11+r16)+0.69*(r3+r12+r17+r21)
+                  + 1*(r4+r8+r9+r13+r22)+1.44*(r5+r10+r14+r18+r23)+2.07*(r15+r19+r24)+3*(r20+r25))/(-3*r0)
+        return output
+
+    def fuzzy_zdz(self, z, dz):
+        secnum = 4
+        dz_max = 1.2
+        everysecdz = dz_max / secnum
+        secdz1 = everysecdz
+        secdz2 = 2 * everysecdz
+        secdz3 = 3 * everysecdz
+
+        z_max = 100
+        everysecz = z_max / secnum
+        secz1 = everysecz
+        secz2 = 2 * everysecz
+        secz3 = 3 * everysecz
+
+        z1 = 1 - z / everysecz
+        z2 = 1 - abs((z - secz1) / everysecz)
+        z3 = 1 - abs((z - secz2) / everysecz)
+        z4 = 1 - abs((z - secz3) / everysecz)
+        z5 = (z - secz3) / everysecz
+
+        z1 = max(min(z1, 1.0), 0.0)
+        z2 = max(z2, 0.0)
+        z3 = max(z3, 0.0)
+        z4 = max(z4, 0.0)
+        z5 = max(min(z5, 1.0), 0.0)
+
+        dz1 = 1 - dz / everysecdz
+        dz2 = 1 - abs((dz - secdz1) / everysecdz)
+        dz3 = 1 - abs((dz - secdz2) / everysecdz)
+        dz4 = 1 - abs((dz - secdz3) / everysecdz)
+        dz5 = (dz - secdz3) / everysecdz
+
+        dz1 = max(min(dz1, 1.0), 0.0)
+        dz2 = max(dz2, 0.0)
+        dz3 = max(dz3, 0.0)
+        dz4 = max(dz4, 0.0)
+        dz5 = max(min(dz5, 1.0), 0.0)
+
+        r1 = min(z1, dz1)
+        r2 = min(z1, dz2)
+        r3 = min(z1, dz3)
+        r4 = min(z1, dz4)
+        r5 = min(z1, dz5)
+        r6 = min(z2, dz1)
+        r7 = min(z2, dz2)
+        r8 = min(z2, dz3)
+        r9 = min(z2, dz4)
+        r10 = min(z2, dz5)
+        r11 = min(z3, dz1)
+        r12 = min(z3, dz2)
+        r13 = min(z3, dz3)
+        r14 = min(z3, dz4)
+        r15 = min(z3, dz5)
+        r16 = min(z4, dz1)
+        r17 = min(z4, dz2)
+        r18 = min(z4, dz3)
+        r19 = min(z4, dz4)
+        r20 = min(z4, dz5)
+        r21 = min(z5, dz1)
+        r22 = min(z5, dz2)
+        r23 = min(z5, dz3)
+        r24 = min(z5, dz4)
+        r25 = min(z5, dz5)
+        r0 = r1 + r2 + r3 + r4 + r5 + r6 + r7 + r8 + r9 + r10 + r11 + r12 + r13 + \
+            r14 + r15 + r16 + r17 + r18 + r19 + r20 + r21 + r22 + r23 + r24 + r25
+
+        # Outputs are 9 sections
+        ratio = math.sqrt(2)
+        zdzoutput = (4.0 * (r1 + r2) + 2.0 * ratio * (r6) + 2.0 * (r3 + r7 + r11) + ratio * (r8 + r9 + r12) +
+                     1.0 * (r4 + r13) + 1 / ratio * (r5 + r16) + 0.5 * (r10 + r14 +
+                                                                        r17 + r18) + 0.5 * ratio * (r15 + r19 + r21 + r22 + r23)
+                     + 0.25 * (r20 + r24 + r25)) / (4.0 * r0)
+
+        return zdzoutput
+
+    def fuzzy_mf(self, f, m):
+        secnum = 4
+        moment_max = 1.5
+        everysecm = moment_max / secnum
+        secm1 = everysecm
+        secm2 = 2 * everysecm
+        secm3 = 3 * everysecm
+
+        force_max = 80
+        everysecf = force_max / secnum
+        secf1 = everysecf
+        secf2 = 2 * everysecf
+        secf3 = 3 * everysecf
+
+        m1 = (m - secm3) / everysecm
+        m2 = 1 - abs(m - secm3) / everysecm
+        m3 = 1 - abs(m - secm2) / everysecm
+        m4 = 1 - abs(m - secm1) / everysecm
+        m5 = 1 - (m - secm1) / everysecm
+
+        m1 = max(min(m1, 1.0), 0.0)
+        m2 = max(m2, 0.0)
+        m3 = max(m3, 0.0)
+        m4 = max(m4, 0.0)
+        m5 = max(min(m5, 1.0), 0.0)
+
+        f1 = (f - secf3) / everysecf
+        f2 = 1 - abs(f - secf3) / everysecf
+        f3 = 1 - abs(f - secf2) / everysecf
+        f4 = 1 - abs(f - secf1) / everysecf
+        f5 = 1 - (f - secf1) / everysecf
+
+        f1 = max(min(f1, 1.0), 0.0)
+        f2 = max(f2, 0.0)
+        f3 = max(f3, 0.0)
+        f4 = max(f4, 0.0)
+        f5 = max(min(f5, 1.0), 0.0)
+
+        # Fuzzy rules
+        r1 = min(m1, f1)
+        r2 = min(m1, f2)
+        r3 = min(m1, f3)
+        r4 = min(m1, f4)
+        r5 = min(m1, f5)
+        r6 = min(m2, f1)
+        r7 = min(m2, f2)
+        r8 = min(m2, f3)
+        r9 = min(m2, f4)
+        r10 = min(m2, f5)
+        r11 = min(m3, f1)
+        r12 = min(m3, f2)
+        r13 = min(m3, f3)
+        r14 = min(m3, f4)
+        r15 = min(m3, f5)
+        r16 = min(m4, f1)
+        r17 = min(m4, f2)
+        r18 = min(m4, f3)
+        r19 = min(m4, f4)
+        r20 = min(m4, f5)
+        r21 = min(m5, f1)
+        r22 = min(m5, f2)
+        r23 = min(m5, f3)
+        r24 = min(m5, f4)
+        r25 = min(m5, f5)
+
+        # Outputs are 9 sections
+        ratio = math.sqrt(2)
+        r0 = r1 + r2 + r3 + r4 + r5 + r6 + r7 + r8 + r9 + r10 + r11 + r12 + r13 + \
+            r14 + r15 + r16 + r17 + r18 + r19 + r20 + r21 + r22 + r23 + r24 + r25
+        mfoutput = (4.0 * (r1 + r2 + r6) + 2.0 * ratio * (r3 + r7 + r8) + 2.0 * (r4 + r9 + r11) + ratio * (r5 + r10 + r12) +
+                    1.0 * (r13 + r16) + 1 / ratio * (r14 + r17 + r21 + r22) +
+                    0.5 * (r15 + r18) + 0.5 * ratio * (r19 + r20 + r23)
+                    + 0.25 * (r24 + r25)) / (4 * r0)
+
+        return mfoutput
+
     def gravitycomp(self, ft, rotate_i):
         '''
          this is a gravit compensation function to compensate the tool gravity and obtain the precise
@@ -118,6 +356,7 @@ class ImpedanceController:
         # pose_noi[0] += 0.001
         self.robot.movel(pose=pose_noi.tolist(), a=1.2, v=1.0)
         print(pose_noi)
+        self.initz = self.robot.get_actual_tcp_pose()[2]
 
     def correct_bias(self):
         ft = np.array(self.sensor.tare()) / 1000000.0
@@ -161,6 +400,7 @@ class ImpedanceController:
         self.v[2] = (err[2]*0.002)/(1+math.exp(abs(ft_base[2])/30))
         self.v[3:6] = -self.v[3:6]
         # print(self.v)
+        self.a = (self.v-self.vd)/self.Tc
         self.vd = self.v
         v_tmp = self.v.tolist()
         v_cmd = [i for item in v_tmp for i in item]
@@ -189,6 +429,58 @@ class ImpedanceController:
         rotate = self.AxisAng2RotaMatri(axisangle)
         ftcomp = self. gravitycomp(ft, np.linalg.inv(rotate))-init_ft
         print(init_ft)
+
+    def get_state_reward(self, variable_name):
+        res = []
+        ft = self.get_ftbase()
+        if variable_name == 'x':
+            res.append(self.v[0, 0])
+            # res.append(self.a[0, 0])
+            res.append(ft[0, 0])
+            z, dz = self.get_zdz()
+            reward = self.fuzzy_reward(ft[0, 0], ft[4, 0], z, dz)
+            res.append(reward)
+        elif variable_name == 'y':
+            res.append(self.v[1, 0])
+            # res.append(self.a[1, 0])
+            res.append(ft[1, 0])
+            z, dz = self.get_zdz()
+            reward = self.fuzzy_reward(ft[1, 0], ft[3, 0], z, dz)
+            res.append(reward)
+        elif variable_name == 'rx':
+            res.append(self.v[3, 0])
+            # res.append(self.a[3, 0])
+            res.append(ft[3, 0])
+            z, dz = self.get_zdz()
+            reward = self.fuzzy_reward(ft[1, 0], ft[3, 0], z, dz)
+            res.append(reward)
+        elif variable_name == 'ry':
+            res.append(self.v[4, 0])
+            # res.append(self.a[4, 0])
+            res.append(ft[4, 0])
+            z, dz = self.get_zdz()
+            reward = self.fuzzy_reward(ft[0, 0], ft[4, 0], z, dz)
+            res.append(reward)
+        # elif variable_name == 'rz':
+        #     res.append(self.v[5, 0])
+        #     # res.append(self.a[5, 0])
+        #     res.append(ft[5, 0])
+        #     z, dz = self.get_zdz()
+        #     reward = self.fuzzy_reward(ft[5, 0], ft[3, 0], z, dz)
+        #     res.append(reward)
+        return res
+
+    def apply_action(self, variable_name, u):
+        if variable_name == 'x':
+            self.B[0, 0] = u
+        elif variable_name == "y":
+            self.B[1, 1] = u
+        elif variable_name == "rx":
+            self.B[3, 3] = u
+        elif variable_name == "ry":
+            self.B[4, 4] = u
+        elif variable_name == "rz":
+            self.B[5, 5] = u
 
 
 if __name__ == "__main__":
