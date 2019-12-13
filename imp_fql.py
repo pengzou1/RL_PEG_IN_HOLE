969c28ba5c68ec9b494a10f8777348b42b94d233
2585924348 <2585924348@qq.com>
2019-12-13 17:15:26 +0800

    modify imp_fql.py

--- imp_fql.py
+++ imp_fql.py
@@ -24,48 +24,69 @@ reward_list = []
 model = FQL.Model(gamma=0.7, alpha=0.5, ee_rate=0.001, past_weight=0.9, q_initial_value='zero',
                   action_set_length=3, fis=fis)
 controller = ImpedanceController()
-for episodes in range(0, 1000):
+step_max = 100
+for episodes in range(1, 1000):
     # if iteration % 1000 == 0 and iteration <= 20000:
     #     env.__init__()
     #     action = model.get_initial_action(env.state)
     #     reward, state_value = env.apply_action(action)
     # action = model.run(state_value, reward)
     # reward, state_value = env.apply_action(action)
+    filepath = '/home/zp/github/RL_PEG_IN_HOLE/data/episodes'+episodes+'.csv'
     controller.__init__()
     controller.move2initpose()
     controller.set_pose_noise()
     controller.correct_bias()
-    while (controller.z < 0.036):
+    state_init, reward = controller.get_state_reward('rx')
+    action = model.get_initial_action(state_init)
+    controller.apply_action('rx', action)
+    for step in range(1, step_max):
+        data_record = []
         ft_base = controller.get_ftbase()
         controller.imp_run(ft_base)
         state_value, reward = controller.get_state_reward('rx')
-        action = model.run(state_value, reward)
-        controller.apply_action('rx', action)
-        # position_list.append(state_value[0])
-        vel_list.append(state_value[1])
-        fh_list.append(state_value[2])
-        action_list.append(action)
-        reward_list.append(reward)
+        if controller.z > 0.036:
+            reward = reward+1-step/step_max
+            action = model.run(state_value, reward)  # update q table
+            data_record.append(state_value[0])
+            data_record.append(state_value[1])
+            data_record.append(action)
+            data_record.append(reward)
+            controller.save2csv(filepath, data_record)
+            break
+        else:
+            action = model.run(state_value, reward)
+            controller.apply_action('rx', action)
+            data_record.append(state_value[0])
+            data_record.append(state_value[1])
+            data_record.append(action)
+            data_record.append(reward)
+            controller.save2csv(filepath, data_record)
+# position_list.append(state_value[0])
+        # vel_list.append(state_value[0])
+        # fh_list.append(state_value[1])
+        # action_list.append(action)
+        # reward_list.append(reward)
 
-plt.figure(1)
-plt.plot(position_list)
-plt.ylabel('position')
-
-
-plt.figure(2)
-plt.plot(vel_list)
-plt.ylabel('vel')
-
-plt.figure(3)
-plt.plot(fh_list)
-plt.ylabel('fh')
-
-plt.figure(4)
-plt.plot(action_list)
-plt.ylabel('damping')
-
-plt.figure(5)
-plt.plot(reward_list)
-plt.ylabel('reward')
-
-plt.show()
+# plt.figure(1)
+# plt.plot(position_list)
+# plt.ylabel('position')
+#
+#
+# plt.figure(2)
+# plt.plot(vel_list)
+# plt.ylabel('vel')
+#
+# plt.figure(3)
+# plt.plot(fh_list)
+# plt.ylabel('fh')
+#
+# plt.figure(4)
+# plt.plot(action_list)
+# plt.ylabel('damping')
+#
+# plt.figure(5)
+# plt.plot(reward_list)
+# plt.ylabel('reward')
+#
+# plt.show()
